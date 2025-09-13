8d9ee3924bd380528f8e1f5c86d054f626fbe01b
Feymovs <saidaurum@gmail.com>
2025-09-13 13:31:34 +0500

    Fix request parsing back

--- tgbot/main.py
+++ tgbot/main.py
@@ -1,22 +1,27 @@
+import asyncio
 import os
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from .handlers import router
+from aiogram import Bot, Dispatcher, Router
+from tgbot.handlers import router
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # https://friday-bot.vercel.app/api/index
 
 class TGBot:
-    def __init__(self, router):
-        self.bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    def __init__(self, router: Router) -> None:
+        self.bot = Bot(TOKEN)
         self.dp = Dispatcher()
         self.dp.include_router(router)
 
-    async def update_bot(self, update_dict):
-        await self.dp.feed_raw_update(self.bot, update_dict)
+        # Устанавливаем webhook один раз при старте
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.set_webhook())
+
+    async def update_bot(self, update: dict) -> None:
+        await self.dp.feed_raw_update(self.bot, update)
+        await self.bot.session.close()
 
     async def set_webhook(self):
         await self.bot.set_webhook(WEBHOOK_URL)
+        await self.bot.session.close()
 
 tgbot = TGBot(router)
