28b41d14cbed5c249056008855cdcc418536ceb4
Feymovs <saidaurum@gmail.com>
2025-09-13 13:25:07 +0500

    Fix request parsing back

--- api/index.py
+++ api/index.py
@@ -2,6 +2,6 @@ import json
 from tgbot.main import tgbot
 
 async def handler(request):
-    update = json.loads(request.body.decode('utf-8'))
+    update = json.loads(request['body'])
     await tgbot.update_bot(update)
     return {'statusCode': 200, 'body': 'ok'}
